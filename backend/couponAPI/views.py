aa19f27a997c2bc141ff0fe7f48efdb43653d4c4
niijima16 <niijima.16@gamil.com>
2025-01-23 23:02:34 +0900

    GE,POST updated 0123T2302

--- backend/couponAPI/views.py
+++ backend/couponAPI/views.py
@@ -12,14 +12,27 @@ class couponSerializers(serializers.Serializer):
     description = serializers.CharField()         # クーポンの詳細
     usage_count = serializers.IntegerField(default=0)  # 使用回数
     max_uses = serializers.IntegerField(default=100)   # 最大使用回数
-    probability = serializers.FloatField(default=0.33)  # 確率（将来調整可能）
+    probability = serializers.FloatField(default=0.33)  # 確率（調整可能）
 
 
 class couponView(APIView):
-
+    # GET request
     def get(self, request):
         coupon_list = Coupon.objects.all()
-
+        # JSONに整形
         serializer = couponSerializers(instance=coupon_list, many=True)
 
         return Response(serializer.data)
+
+    # POST request
+    def post(self, request):
+        # JSONからオブジェクトに整形
+        print("data", request.data)
+        serializer = couponSerializers(data=request.data)
+        # データチェック
+        if serializer.is_valid():
+            new_data = Coupon.objects.create(**serializer.validated_data)
+            return Response(serializer.data) # クリア時DB更新
+        else:
+            return Response(serializer.errors) # エラーの場合エラーを返す
+        
