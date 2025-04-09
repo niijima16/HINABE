16e7d0e4125d534fe487f60ee0536f91576dc9cd
niijima16 <niijima.16@gamil.com>
2025-04-09 22:53:54 +0900

    add react code 20250409T2253

---
+++ frontend/hinabe-front/src/App.jsx
@@ -0,0 +1,13 @@
+// App.jsx
+
+import './App.css';
+
+function App() {
+    return (
+        <div>
+            <h1>こんにちは</h1>
+        </div>
+    );
+}
+
+export default App;
---
+++ frontend/react/main.js
@@ -0,0 +1,5 @@
+bbbaaa
+bbbaaa
+aaaaaa
+aaabbb
+aaaaaa
